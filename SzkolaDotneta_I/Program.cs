2d949ab2796b992ccc1fb4449ec234591b6c84f4
pajusco <pajus1337@gmail.com>
2023-10-26 12:05:45 +0200

    init commit

---
+++ SzkolaDotneta_I/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace SzkolaDotneta_I
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
