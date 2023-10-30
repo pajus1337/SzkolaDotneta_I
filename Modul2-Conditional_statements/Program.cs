c87a46c531e5ba81728dbeb1d79b7af7664c7b61
PaJuS <pajus1337@gmail.com>
2023-10-30 14:18:47 +0100

    Project update : - Code added for task 3,4.

--- Modul2-Conditional_statements/Program.cs
+++ Modul2-Conditional_statements/Program.cs
@@ -7,7 +7,9 @@ namespace Modul2_Conditional_statements
         static void Main(string[] args)
         {
             TaskOne(5, 5);
-            TaskTwo(11);
+            TaskTwo(15);
+            TaskThree();
+            TaskFour();
         }
 
         private static void TaskOne(int a, int b)
@@ -32,14 +34,54 @@ namespace Modul2_Conditional_statements
 
         private static void TaskTwo(int a)
         {
-            Console.WriteLine("Task Two :");
             int numberA = a;
+
+            Console.WriteLine("Task Two :");
             Console.WriteLine(numberA%2 == 0 ? $"the given number {numberA} is an even number" : $"the given number {numberA} is an odd number");
         }
 
         private static void TaskThree()
         {
+            int numberUserEntered = default;
+            string userInput = string.Empty;
 
+            Console.WriteLine("Task Three :");
+            Console.WriteLine("Enter the number you want to check (whether it is negative or positive)");
+            userInput = Console.ReadLine();
+            if (!int.TryParse(userInput,out numberUserEntered) || ( numberUserEntered == 0))
+            {
+                Console.WriteLine("The wrong sign was entered, or the digit 0, all numbers greater than or less than 0 are allowed.");
+            }
+            else
+            {
+                Console.WriteLine(numberUserEntered > 0 ? $"the entered number {numberUserEntered} is a positive number" : $"the entered number {numberUserEntered} is a negative number");
+            }
+        }
+
+        private static void TaskFour()
+        {
+            int yearUserEntered = default;
+            string userInput = string.Empty;
+
+            Console.WriteLine("Task Four :");
+            Console.WriteLine("Enter the year to check if it is a leap year");
+            userInput = Console.ReadLine();
+
+            if (int.TryParse(userInput, out yearUserEntered))
+            {
+                if ((yearUserEntered % 4 == 0 && yearUserEntered % 100 != 0) || (yearUserEntered % 400 == 0))
+                {
+                    Console.WriteLine($"Entered year {yearUserEntered} is a leap year");
+                }
+                else
+                {
+                    Console.WriteLine($"Entered year {yearUserEntered} is not a leap year");
+                }
+            }
+            else
+            {
+                Console.WriteLine($"the entered value {userInput} is incorrect, check the correctness and try again.");
+            }
         }
     }
 }
