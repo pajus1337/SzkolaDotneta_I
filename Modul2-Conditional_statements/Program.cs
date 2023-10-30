10e86177169da12a001a2022c6e32c88ad52accb
PaJuS <pajus1337@gmail.com>
2023-10-30 06:59:36 +0100

    Init commit New project dedicated to solving tasks related to : - Conditional statements

---
+++ Modul2-Conditional_statements/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Modul2_Conditional_statements
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
