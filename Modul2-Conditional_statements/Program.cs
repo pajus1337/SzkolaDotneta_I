a4020aca7bc360f4920c6a525efbf7bd0b2531dc
PaJuS <pajus1337@gmail.com>
2023-10-30 07:55:25 +0100

    Project Update : - Created a file "TaskContent.txt" with the content of the tasks to be performed - Code added for first 2 tasks

--- Modul2-Conditional_statements/Program.cs
+++ Modul2-Conditional_statements/Program.cs
@@ -1,10 +1,45 @@
-﻿namespace Modul2_Conditional_statements
+﻿using static System.Runtime.InteropServices.JavaScript.JSType;
+
+namespace Modul2_Conditional_statements
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            TaskOne(5, 5);
+            TaskTwo(11);
+        }
+
+        private static void TaskOne(int a, int b)
+        {
+            int numberA = a;
+            int numberB = b;
+
+            Console.WriteLine("Task One :");
+            if (numberA > numberB)
+            {
+                Console.WriteLine($"the number {numberA} a is greater than the number {numberB}");
+            }
+            else if (numberA < numberB)
+            {
+                Console.WriteLine($"the number {numberA} is smaller than the number {numberB}");
+            }
+            else
+            {
+                Console.WriteLine($"number {numberA} and number {numberB} are equal");
+            }
+        }
+
+        private static void TaskTwo(int a)
+        {
+            Console.WriteLine("Task Two :");
+            int numberA = a;
+            Console.WriteLine(numberA%2 == 0 ? $"the given number {numberA} is an even number" : $"the given number {numberA} is an odd number");
+        }
+
+        private static void TaskThree()
+        {
+
         }
     }
 }
