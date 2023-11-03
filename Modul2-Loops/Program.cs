51418e70ce210009277b499c2c31053329299c6b
pajusco <pajus1337@gmail.com>
2023-11-03 13:14:54 +0100

    Init commit of new project

---
+++ Modul2-Loops/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Modul2_Loops
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
