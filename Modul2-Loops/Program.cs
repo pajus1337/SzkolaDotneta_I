ce10c0b9b4a1cb37e8d2a4ad58d98c772220ee1a
pajusco <pajus1337@gmail.com>
2023-11-04 18:43:49 +0100

    Project update : - Added code ( Solution ) for task 1

--- Modul2-Loops/Program.cs
+++ Modul2-Loops/Program.cs
@@ -1,10 +1,39 @@
-﻿namespace Modul2_Loops
+﻿
+
+using System.Collections.Concurrent;
+
+namespace Modul2_Loops
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            TaskOne();
+        }
+
+        private static void TaskOne()
+        {
+            int currentNumber = 2;
+            int amountOfPrimNummbers = default;
+
+            do
+            {
+                for (int i = 1; i <= currentNumber; i++)
+                {
+
+                    if ((currentNumber % i == 0) && (i != 1 && i != currentNumber))
+                    {
+                        break;
+                    }
+                    else if (i == currentNumber - 1)
+                    {
+                        amountOfPrimNummbers++;
+                    }
+                }
+                currentNumber++;
+
+            } while (currentNumber != 100);
+            Console.WriteLine($"Amount of Prime Numbers in range 1-100 = {amountOfPrimNummbers}");
         }
     }
 }
