a1443b37849074e26229c1516fd2b97ad5096bb0
pajusco <pajus1337@gmail.com>
2023-11-05 12:30:16 +0100

    Project update : - Added code ( solution ) for task 2 & 3 - Subtle visual corrections

--- Modul2-Loops/Program.cs
+++ Modul2-Loops/Program.cs
@@ -1,39 +1,76 @@
-﻿
-
-using System.Collections.Concurrent;
+﻿using System.Text;
+using System.Threading.Channels;
+using static System.Net.Mime.MediaTypeNames;
 
 namespace Modul2_Loops
 {
     internal class Program
     {
         static void Main(string[] args)
-        {
+        {          
             TaskOne();
+            TaskTwo();
+            TaskThree();
         }
 
         private static void TaskOne()
         {
             int currentNumber = 2;
-            int amountOfPrimNummbers = default;
+            int amountOfPrimNumbers = default;
+
+            Console.WriteLine("Task One :");
 
             do
             {
                 for (int i = 1; i <= currentNumber; i++)
                 {
-
                     if ((currentNumber % i == 0) && (i != 1 && i != currentNumber))
                     {
                         break;
                     }
                     else if (i == currentNumber - 1)
                     {
-                        amountOfPrimNummbers++;
+                        amountOfPrimNumbers++;
                     }
                 }
                 currentNumber++;
-
             } while (currentNumber != 100);
-            Console.WriteLine($"Amount of Prime Numbers in range 1-100 = {amountOfPrimNummbers}");
+            Console.WriteLine($"Amount of prime numbers in range 1-100 = {amountOfPrimNumbers}");
+        }
+
+        private static void TaskTwo()
+        {        
+            StringBuilder stringBuilder = new StringBuilder();
+            int i = 1;
+
+            Console.WriteLine("Task Two : ");
+
+            do
+            {                
+                if (i%2 == 0) 
+                {
+                    stringBuilder.Append(i + " ");
+                }
+                i++;
+            } while (i <= 1000);
+            Console.WriteLine($"Even numbers in the range 1-1000 : {stringBuilder}");
+        }
+
+        private static void TaskThree()
+        {
+            int a = 0;
+            int b = 1;
+            int c = 0;
+
+            Console.WriteLine("Task Three : ");
+
+            for (int i = 0; i <= 10; i++)
+            {
+                Console.WriteLine($"F{i} = {c} ");
+                a = b;
+                b = c;
+                c = a + b;                
+            }
         }
     }
 }
